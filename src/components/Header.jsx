4b487669ca5eb4f464023cd827936b7ba0bb12a5
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:16:01 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <div id="main">
+        <div id="para1"></div>
+        <div id="para2"></div>
+        <div id="para3"></div>
+      </div>
+      <br />
+      <h1>Flag of Armenia</h1>
+    </>
+  );
+}
